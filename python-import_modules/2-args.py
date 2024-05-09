7dcc381cf1297af595d45823299aed6f6e05a3a0
Ezio-33 <8691@holbertonstudents.com>
2024-05-09 23:38:06 +0200

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    print(len(argv) - 1, "arguments", end='')
+    if len(argv) == 1:
+        print(".", end='')
+    else:
+        print(":", end='')
+    print()
+    if len(argv) > 1:
+        for i in range(1, len(argv)):
+            print(i, ':', argv[i])
