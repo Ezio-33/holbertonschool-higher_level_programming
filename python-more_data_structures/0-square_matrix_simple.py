e206f9d86f595f18f1adc40c736368eead6d8162
Ezio-33 <8691@holbertonstudents.com>
2024-05-15 15:37:55 +0200

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0] * len(matrix[0]) for _ in matrix]
-    for i, row in enumerate(matrix):
-        for j, elem in enumerate(row):
-            new_matrix[i][j] = elem ** 2
-    return new_matrix
+    new = matrix.copy()
+    for cpt in range(len(matrix)):
+        new[cpt] = list(map(lambda x: x**2, matrix[cpt]))
+
+    return (new)
