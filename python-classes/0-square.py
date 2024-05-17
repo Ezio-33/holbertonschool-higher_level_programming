95d2427c8f0dd77556cdc3e2e221c1341d94cac3
Ezio-33 <8691@holbertonstudents.com>
2024-05-17 13:35:13 +0200

    task 0 et 1

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Écrit une classe vide """
+
+
+class Square:
+    """
+    Classe qui crée un carré
+    """
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Initialise la classe Square avec une taille donnée en paramètre.
+"""
+
+
+class Square:
+    """
+    Paramètres:
+    size (int): La taille du côté du carré.
+    """
+    def __init__(self, size):
+        self.__size = size
