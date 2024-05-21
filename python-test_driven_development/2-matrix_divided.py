269ac6c4b12d56d6711359308021291d82f44432
Ezio-33 <8691@holbertonstudents.com>
2024-05-21 14:08:51 +0200

    Correction pycode

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,29 +8,37 @@ def matrix_divided(matrix, div):
     div (int ou float): Le diviseur.
 
     Return:
-    list de listes de float: Une nouvelle matrice avec les éléments divisés par le diviseur.
+    list de listes de float: Une nouvelle matrice avec les éléments
+    divisés par le diviseur.
 
-    Raises: :  TypeError: Si la matrice n'est pas une liste de listes d'entiers ou de flottants,
+    Raises: :  TypeError: Si la matrice n'est pas une liste de listes
+    d'entiers ou de flottants,
                ou si chaque ligne de la matrice n'a pas la même taille,
                ou si le diviseur n'est pas un nombre.
     ZeroDivisionError: Si le diviseur est égal à zéro.
-    
+
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+            )
+
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) "
+                    "of integers/floats"
+                    )
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     return [[round(element / div, 2) for element in row] for row in matrix]
