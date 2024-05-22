c4d23b2c4b8591efaa472d3201e8dae9d6d05981
Ezio-33 <8691@holbertonstudents.com>
2024-05-22 10:03:16 +0200

    task 5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
+class TestMaxInteger(unittest.TestCase):
+    def test_liste_vide(self):
+        self.assertIsNone(max_integer([]))
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+    def test_liste_un_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_nombres_positifs(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_nombres_negatifs(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
+
+    def test_nombres_melanges(self):
+        self.assertEqual(max_integer([-1, 2, -3, 4, -5]), 4)
+
+    def test_nombres_dupliques(self):
+        self.assertEqual(max_integer([1, 2, 3, 3, 2, 1]), 3)
+
+    def test_grands_nombres(self):
+        self.assertEqual(max_integer([10**10, 10**11, 10**12]), 10**12)
