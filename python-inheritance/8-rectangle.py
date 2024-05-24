2f6e75dd55300565831b64f7c318ae0184a2e341
Ezio-33 <8691@holbertonstudents.com>
2024-05-24 18:31:29 +0200

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Définit une classe Rectangle qui hérite de BaseGeometry (7-base_geometry.py)."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Classe Rectangle qui hérite de BaseGeometry"""
+
+    def __init__(self, width, height):
+        """
+        Initialise une nouvelle instance de Rectangle.
+
+        Args:
+            width (int): La largeur du rectangle.
+            height (int): La hauteur du rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
