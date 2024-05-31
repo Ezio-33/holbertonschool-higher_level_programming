4a40f0fa27fde25ede1750bbd017c431a3858dac
Ezio-33 <8691@holbertonstudents.com>
2024-05-31 02:53:51 +0200

    task 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import json
+
+PORT = 8000
+
+
+class Http_Requet(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            status = {"status": "OK"}
+            self.wfile.write(json.dumps(status).encode('utf-8'))
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            info = {"version": "1.0", "description":
+                    "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            error_message = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+
+
+"""Configuration et démarrage du serveur"""
+with socketserver.TCPServer(("", PORT), Http_Requet) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
