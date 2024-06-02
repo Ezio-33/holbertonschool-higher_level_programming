1258d5d71232a9a99ce4740bd4a55d370c6f73de
Ezio-33 <8691@holbertonstudents.com>
2024-06-02 17:21:54 +0200

    task 0

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-import http.server
-import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
-PORT = 8000
 
-
-class HttpRequestHandler(http.server.BaseHTTPRequestHandler):
+class HttpRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -21,26 +18,21 @@ class HttpRequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            status = {"status": "OK"}
-            self.wfile.write(json.dumps(status).encode('utf-8'))
+            self.wfile.write(b"OK")
         elif self.path == '/info':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            info = {"version": "1.0", "description":
-                    "A simple API built with http.server"}
-            self.wfile.write(json.dumps(info).encode('utf-8'))
+            self.wfile.write(b'{"version": "1.0", "description": "A simple API built with http.server"}')
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            error_message = {"404 Not Found"}
-            self.wfile.write(json.dumps(error_message).encode('utf-8'))
+            self.wfile.write(b"404 Not Found")
 
 
 """Configuration et démarrage du serveur"""
-with socketserver.TCPServer(("", PORT), HttpRequestHandler) as httpd:
-    print(f"Serving on port {PORT}")
-    httpd.serve_forever()
+httpd = HTTPServer(('', 8000), HttpRequestHandler)
+httpd.serve_forever()
--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,33 +1,58 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 from flask import Flask, jsonify, request
 
+""" Crée une instance de l'application Flask"""
 app = Flask(__name__)
 
-users = {
-    "jane": {"username": "jane", "name": "Jane",
-             "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John",
-             "age": 30, "city": "New York"}
-}
+""" Initialise un dictionnaire vide pour stocker les utilisateurs"""
+users = {}
 
 
-@app.route('/')
+@app.route("/")
 def home():
+    """
+    Cette fonction gère la route racine de l'API Flask.
+    Return:
+            str: Un message de bienvenue pour l'API Flask.
+    """
     return "Welcome to the Flask API!"
 
 
-@app.route('/data')
-def get_data():
+@app.route("/data")
+def data():
+    """
+    Récupère une liste de noms d'utilisateur du dictionnaire 'users'
+    et la retourne sous forme de réponse JSON.
+    Return:
+            Une réponse JSON contenant une liste de noms d'utilisateur.
+    """
     return jsonify(list(users.keys()))
 
 
-@app.route('/status')
+@app.route("/status")
 def status():
+    """
+    Retourne le statut de l'API.
+    Return:
+            str: Le message de statut "OK".
+    """
     return "OK"
 
 
-@app.route('/users/<username>')
-def get_user(username):
+@app.route("/users/<username>")
+def user(username):
+    """
+    Récupère les informations de l'utilisateur
+    basé sur le nom d'utilisateur fourni.
+    Args:
+            username: Le nom d'utilisateur de l'utilisateur
+            pour lequel récupérer les informations.
+    Return:
+            Si l'utilisateur est trouvé, les informations de l'utilisateur
+            sont retournées sous forme de réponse JSON.
+            Si l'utilisateur n'est pas trouvé, une réponse JSON avec un
+            message d'erreur et un code de statut 404 est retournée.
+    """
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -35,15 +60,30 @@ def get_user(username):
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.get_json()
-    username = data.get('username')
+    """
+    Ajoute un nouvel utilisateur au système.
+    Return:
+            Une réponse JSON avec la structure suivante:
+            - Si les données JSON sont invalides:
+            {"error": "Invalid JSON data"}
+            - Si le nom d'utilisateur existe déjà:
+            {"error": "Username already exists"}
+            - Si l'utilisateur est ajouté avec succès:
+            {"message": "User added", "user": <user_data>}
+            - Si une exception se produit: {"error": <exception_message>}
+    """
+    new_user = request.get_json()
+    username = new_user.get("username")
+    if not username:
+        return jsonify({"error": "username is required"}), 400
     if username in users:
-        return jsonify({"error": "User already exists"}), 400
-    users[username] = data
-    return jsonify({"message": "User added", "user": data})
+        return jsonify({"error": "Username already exists"}), 409
+    users[username] = new_user
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 
+""" Démarre le serveur Flask lorsque le script est exécuté directement"""
 if __name__ == "__main__":
     app.run()
