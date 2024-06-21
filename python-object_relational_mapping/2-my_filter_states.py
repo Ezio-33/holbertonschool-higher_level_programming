5695c7dfcd83e248faa9ab93fa12c5d3462f49f7
Ezio-33 <8691@holbertonstudents.com>
2024-06-22 00:01:30 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
-Args:
-username (str): The username for the MySQL database.
-password (str): The password for the MySQL database.
-database (str): The name of the MySQL database.
+This script takes in an argument and displays all values in the states table
+of the database hbtn_0e_0_usa where the name matches the argument.
+
+Arguments:
+    mysql_username: Your MySQL username.
+    mysql_password: Your MySQL password.
+    database_name: The name of the database to connect to.
+    state_name: The name of the state to search for.
+
+The script connects to a MySQL server running on localhost at port 3306
+and fetches all rows in the `states` table with the name matching the argument,
+sorted in ascending order by `id`.
 """
+
 if __name__ == "__main__":
     import sys
     import MySQLdb
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name_searched = sys.argv[4]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
@@ -24,9 +31,10 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    select = """SELECT * FROM states WHERE name = '{}'
-ORDER BY id ASC""".format(state_name_searched)
-    cursor.execute(select)
+    query = """SELECT * FROM states WHERE
+    BINARY name = '{}' ORDER BY id ASC""".format(
+        state_name)
+    cursor.execute(query)
     states = cursor.fetchall()
 
     for state in states:
