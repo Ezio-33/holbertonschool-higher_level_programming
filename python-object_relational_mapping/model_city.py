9c8397eb9552bd121db23bdf0c7ddd65438104d9
Ezio-33 <8691@holbertonstudents.com>
2024-06-21 23:51:14 +0200

    task 6

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,18 +4,12 @@ Définition de la classe City et instance Base = declarative_base().
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from model_state import Base, State
+from model_state import Base
 
 
 class City(Base):
-    """
-    Classe City qui hérite de Base.
-    Représente la table 'cities' dans la base de données.
-    """
+    """State class"""
     __tablename__ = 'cities'
-
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,21 +3,14 @@
 Définition de la classe State et
 instance Base = declarative_base().
 """
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    Classe State qui hérite de Base.
-    Représente la table 'states' dans la base de données.
-    """
+    """State class"""
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state")
