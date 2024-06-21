3ccc67763d6c4277a10a37a2ee6d8e9c4e17f54d
Ezio-33 <8691@holbertonstudents.com>
2024-06-21 23:57:30 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,34 +5,32 @@ affiche toutes les valeurs de la table "states"
 où le nom correspond à l'argument fourni.
 
 Args :
-    nom_utilisateur (str) : Le nom d'utilisateur de la base de données MySQL.
-    mdp (str) : Le mot de passe de la base de données MySQL.
-    database (str) : Le nom de la base de données.
-    nom_etat (str) : Le nom de l'état à rechercher.
+username (str): Le nom d'utilisateur pour la base de données MySQL.
+password (str): Le mot de passe de la base de données MySQL.
+database (str): Le nom de la base de données MySQL.
 """
 
 
 if __name__ == "__main__":
     import sys
     import MySQLdb
-    nom_utilisateur = sys.argv[1]
-    mdp = sys.argv[2]
+    username = sys.argv[1]
+    password = sys.argv[2]
     database = sys.argv[3]
-    nom_etat = sys.argv[4]
+    state_name_searched = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=nom_utilisateur,
-        passwd=mdp,
+        user=username,
+        passwd=password,
         db=database
     )
 
     cursor = db.cursor()
     select = """SELECT * FROM states WHERE name = '{}'
-            ORDER BY id ASC""".format(nom_etat)
+ORDER BY id ASC""".format(state_name_searched)
     cursor.execute(select)
-
     states = cursor.fetchall()
 
     for state in states:
